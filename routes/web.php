196843daea295cdff5d38c265da9bb4307e812ae
JJuanMota <juanmotagt@gmail.com>
2022-05-26 12:54:30 -0300

    Criação de rotas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+Route::get('/novo', [App\Http\Controllers\CampeonatoController::class, 'novoCampeonato'])->name('novoCampeonato');
+Route::post('/createCamp', [App\Http\Controllers\CampeonatoController::class, 'novoCampeonato'])->name('createCamp');
+
