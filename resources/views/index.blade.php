ede6efb14260a6ee27e05c711bee7feddde1bbda
JJuanMota <juanmotagt@gmail.com>
2022-05-25 21:04:41 -0300

    commit blade para classificação de jogos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('content')
+    <section class="tabela-jogos">
+        <div class="col-md-12 p-3 bg-dark">
+            <div class="row">
+                <div class="col-md-3 card text-white bg-light mb-3 border-right border-light">
+                    <a href=""><div class="card-header">Time</div></a>
+                    <a href=""><div class="card-body">Time</div></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 ml-md-auto card text-white bg-light mb-3" style="max-width: 18rem;">
+                    <a href=""><div class="card-header">Time</div></a>
+                    <a href=""><div class="card-body">Time</div></a>
+                </div>
+                <div class="col-md-3 ml-md-auto"></div>
+            </div>
+            <div class="row">
+                <div class="col-auto col-md-3 mr-auto card text-white bg-light mb-3" style="max-width: 18rem;">
+                    <a href=""><div class="card-header">Time</div></a>
+                    <a href=""><div class="card-body">Time</div></a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-3 ml-md-auto"></div>
+                <div class="col-md-3 ml-md-auto card text-white bg-light mb-3 border-right border-light">
+                    <a href=""><div class="card-header">Time</div></a>
+                    <a href=""><div class="card-body">Time</div></a>
+                </div>
+            </div>
+            <br><br>
+
+
+            <div class="row">
+                <div class="col-md-3 card text-white bg-light mb-3 border-right border-light">
+                    <a href=""><div class="card-header">Time</div></a>
+                    <a href=""><div class="card-body">Time</div></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 ml-md-auto card text-white bg-light mb-3" style="max-width: 18rem;">
+                    <a href=""><div class="card-header">Time</div></a>
+                    <a href=""><div class="card-body">Time</div></a>
+                </div>
+                <div class="col-md-3 ml-md-auto"></div>
+            </div>
+            <div class="row">
+                <div class="col-auto col-md-3 mr-auto card text-white bg-light mb-3" style="max-width: 18rem;">
+                    <a href=""><div class="card-header">Time</div></a>
+                    <a href=""><div class="card-body">Time</div></a>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
