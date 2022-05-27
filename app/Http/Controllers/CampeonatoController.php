3aa3b0eba4c8d37290b515c453e251c4d93b3940
JJuanMota <juanmotagt@gmail.com>
2022-05-26 22:35:15 -0300

    preparando backend para receber arquivo python

--- app/Http/Controllers/CampeonatoController.php
+++ app/Http/Controllers/CampeonatoController.php
@@ -4,27 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\Campeonato;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CampeonatoController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function novoCampeonato()
     {
         return view('novoCampeonato');
     }
 
-    public function createCamp(Request $request)
+    public function createCampeonato(Request $request)
     {
         $campeonato = new Campeonato();
-        $campeonato->nome = $request->nome;
-        $campeonato->time_1 = $request->time_1;
-        $campeonato->time_2 = $request->time_2;
-        $campeonato->time_3 = $request->time_3;
-        $campeonato->time_4 = $request->time_4;
-        $campeonato->time_5 = $request->time_5;
-        $campeonato->time_6 = $request->time_6;
-        $campeonato->time_7 = $request->time_7;
-        $campeonato->time_8 = $request->time_8;
+        $campeonato->nome       = $request->nome;
+        $campeonato->user_id    = Auth::user()->id;
+        $campeonato->time_1     = $request->time_1;
+        $campeonato->time_2     = $request->time_2;
+        $campeonato->time_3     = $request->time_3;
+        $campeonato->time_4     = $request->time_4;
+        $campeonato->time_5     = $request->time_5;
+        $campeonato->time_6     = $request->time_6;
+        $campeonato->time_7     = $request->time_7;
+        $campeonato->time_8     = $request->time_8;
         $campeonato->save();
-        return $this->novoCampeonato();
+        return redirect('/simula');
+    }
+
+    public function simulaCampeonato()
+    {
+        $campeonatos = Campeonato::where('user_id',Auth::user()->id)->get();
+        return view('index',[
+            'campeonatos' => $campeonatos
+        ]);
+    }
+
+    public function geraCampeonato(Request $request)
+    {
+
+        return false;
     }
 }
