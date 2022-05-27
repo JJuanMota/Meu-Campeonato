b916e734ea516e650891ba0e1294494d2588ab39
JJuanMota <juanmotagt@gmail.com>
2022-05-26 22:36:41 -0300

    alterando blade para simulação de campeonato

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,22 @@
 @section('content')
     <section class="select-camp">
         <div class="container">
-            <div class="row">
-                <select class="form-control form-control-sm">
+            <h2 class="text-center">Simular Campeonatos</h2>
+            <div class="row mt-4">
+                <select id="campeonato" name="campeonato" class="form-control form-control-sm">
                     <option>Selecione um Campeonato</option>
+                    @foreach($campeonatos as $camp)
+                        <option value="{{$camp->id}}">{{$camp->nome}}</option>
+                    @endforeach
                 </select>
             </div>
         </div>
     </section>
-    <section class="tabela-jogos bg-dark d-none">
+    <section id="tabela" class="tabela-jogos bg-dark d-none">
         <div class="container">
             <div class="col-md-12 p-3">
                 <div class="row">
-                    <div class="col-md-2 card text-white bg-light mb-3 border-right border-light">
+                    <div class="col-md-3 card text-white bg-light mb-3 border-right border-light">
                         <a href=""><div class="card-header">Time</div></a>
                         <a href=""><div class="card-body">Time</div></a>
                     </div>
@@ -42,7 +46,7 @@
                 <br><br>
 
                 <div class="row">
-                    <div class="col-md-2 card text-white bg-light mb-3 border-right border-light">
+                    <div class="col-md-3 card text-white bg-light mb-3 border-right border-light">
                         <a href=""><div class="card-header">Time</div></a>
                         <a href=""><div class="card-body">Time</div></a>
                     </div>
@@ -63,4 +67,29 @@
             </div>
         </div>
     </section>
+@endsection
+@section('scripts')
+    <script type="text/javascript">
+        $("#campeonato").change(function () {
+
+            $.ajax({
+                url: "{{route('geraCampeonato')}}",
+                type: "POST",
+                data: {
+                    'id' : $("#campeonato").val(),
+                    '_token': '{{csrf_token()}}'
+                },
+                dataType: 'JSON',
+                success: function(response) {
+
+                    $("#tabela").removeClass('d-none');
+                },
+                error: function(response) {
+
+                }
+
+            });
+        });
+
+    </script>
 @endsection
--- resources/views/novoCampeonato.blade.php
+++ resources/views/novoCampeonato.blade.php
@@ -2,18 +2,18 @@
 @section('content')
     <section class="tabela-jogos">
         <div class="container">
-            <h2>Novo Campeonato</h2>
-            <br><br>
-            <div class="justify-content-center">
+            <h2 class="text-center">Novo Campeonato</h2>
+            <div class="row justify-content-center">
+                <br><br>
                 <form method="POST" action="{{route('createCamp')}}">
                     @csrf
-                    <h5 class="text-secondary">Digite o nome dos times</h5>
-                    <div class="row">
+                    <h5 class="text-center text-secondary">Digite o nome dos times</h5>
+                    <div class="row justify-content-center">
                         <div class="form-group col-md-8">
                             <input required type="text" class="form-control" placeholder="Insira o nome do seu campeonato" name="nome">
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row justify-content-center">
                         <div class="form-group col-md-4">
                             <input required type="text" class="form-control" placeholder="Time 1" name="time_1">
                         </div>
@@ -21,7 +21,7 @@
                             <input required type="text" class="form-control" placeholder="Time 2" name="time_2">
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row justify-content-center">
                         <div class="form-group col-md-4">
                             <input required type="text" class="form-control" placeholder="Time 3" name="time_3">
                         </div>
@@ -29,7 +29,7 @@
                             <input required type="text" class="form-control" placeholder="Time 4" name="time_4">
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row justify-content-center">
                         <div class="form-group col-md-4">
                             <input required type="text" class="form-control" placeholder="Time 5" name="time_5">
                         </div>
@@ -37,7 +37,7 @@
                             <input required type="text" class="form-control" placeholder="Time 6" name="time_6">
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row justify-content-center">
                         <div class="form-group col-md-4">
                             <input required type="text" class="form-control" placeholder="Time 7" name="time_7">
                         </div>
@@ -45,7 +45,9 @@
                             <input required type="text" class="form-control" placeholder="Time 8" name="time_8">
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-success">Enviar</button>
+                    <div class="row justify-content-center">
+                        <button type="submit" class=" btn btn-success">Enviar</button>
+                    </div>
                 </form>
             </div>
         </div>
